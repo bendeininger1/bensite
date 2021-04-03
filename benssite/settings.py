e31a77dbcc141fa284780ea41ddbb92d7f07b80b
bendeininger1 <57870564+bendeininger1@users.noreply.github.com>
2021-04-03 14:11:08 -0400

    update app to use PostgreSQL

--- benssite/settings.py
+++ benssite/settings.py
@@ -76,11 +76,25 @@ WSGI_APPLICATION = 'benssite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'django',
+
+        'USER': 'django',
+
+        'PASSWORD': 'password',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
+
 
 
 # Password validation
