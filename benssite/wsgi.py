f8472933241e1361886be211727d6369acf4b159
bendeininger1 <57870564+bendeininger1@users.noreply.github.com>
2021-04-11 14:36:14 -0400

    Created production.py and updated wsgi.py to allow deployment to azure

--- benssite/wsgi.py
+++ benssite/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'benssite.settings')
+# If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
+# on Azure App Service and should use the production settings in production.py.
+settings_module = "benssite.production" if 'WEBSITE_HOSTNAME' in os.environ else 'benssite.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
